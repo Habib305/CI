c2cdd4ac0f6776bbfd1ec903defc1b759ccbd79b
Habib <habib.alhabib@crok4it.com>
2024-06-08 01:54:22 +0200

    Initial commit

---
+++ ci/src/main/java/com/habib/ci/CiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.habib.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiIntegrationApplication.class, args);
+	}
+
+}
